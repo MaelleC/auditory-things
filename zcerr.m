715d8c594f7356ba41668681b5bcbc240a2821be
Maelle Colussi <maelle.colussi@epfl.ch>
2013-05-17 14:51:19 +0200

    better data frequ mode tone

--- zcerr.m
+++ zcerr.m
@@ -1,20 +1,23 @@
 function [errorProp] = zcerr(maxthings, baseline)
 
+%correct this function
+
 %http://en.wikipedia.org/wiki/Propagation_of_uncertainty#Example_formulas
 
 %f = a - b
-sigmaA1 = zcstdofmean(maxthings);
+sigmaA1 = std(maxthings);
 sigmaB1 = zcstdofmean(baseline);
-covA1B1 = 0;
+covA1B1 = [];
 %http://www.talkstats.com/showthread.php/19836-Covariance-Matrix-of-Sample-Means
 
-if 1 == 0
+
 for indexm=1:1:length(maxthings)
+	covpart = 0;
 	for indexb=1:1:length(baseline)
-		covA1B1 = covA1B1 + (maxthings(indexm) - mean(maxthings))*(baseline(indexb) - mean(baseline));
+		covpart = covpart + (maxthings(indexm) - mean(maxthings))*(baseline(indexb) - mean(baseline));
 	end
-end
-covA1B1 = covA1B1/(length(baseline) * length(maxthings))
+	covpart = covpart /length(baseline); %ok this division ? (will be float ?)
+	covA1B1 = [covA1B1 covpart];
 end
 
 varianceOfSub = sigmaA1^2 + sigmaB1^2 - 2*covA1B1;
@@ -22,21 +25,11 @@ varianceOfSub = sigmaA1^2 + sigmaB1^2 - 2*covA1B1;
 %f = a/b
 sigmaA2 = sqrt(varianceOfSub);
 sigmaB2 = zcstdofmean(baseline);
-A2 = mean(maxthings) - mean(baseline);
+A2 = maxthings - mean(baseline);
 B2 = mean(baseline);
-covA2B2 = 0;
+covA2B2 = 0;%since, B2 is only one number that is its own expectation
 
-if 1 == 0
-for indexm=1:1:length(A2)
-	for indexb=1:1:length(baseline)
-		covA2B2 = covA2B2 + (A2(indexm) - mean(A2))*(baseline(indexb) - mean(baseline));
-	end
-end
-covA2B2 = covA2B2/(length(baseline) * length(maxthings))
-end
-
-%corrA2B2 = covA2B2/(sigmaA2 * sigmaB2);
 F = A2/B2;
-varianceOfDiv = F^2 *( (sigmaA2 /A2)^2 + (sigmaB2/ B2)^2 - 2 * covA2B2 /(A2 * B2));
+varianceOfDiv = F^2 .*( (sigmaA2 ./A2)^2 + (sigmaB2/ B2)^2 - 2 * covA2B2 ./(A2 * B2));
 
-errorProp = sqrt(varianceOfDiv);
+errorProp = sqrt(mean(varianceOfDiv));
