345a52833eac4d657b20d2c92ab2640d1af48ef5
Maelle Colussi <maelle.colussi@epfl.ch>
2013-05-29 10:11:50 +0200

    243 psth

--- zcfrequ_psth.m
+++ zcfrequ_psth.m
@@ -53,21 +53,72 @@ if doonlygraph == 0
 	end
 else
 	if fouriergraph == 1
+		for takeAbs=0:1:1
+			for calc=0:1:3
+			
+				normal = [];
+				noref = [];
+				
+				for index=1:1:length(frequs)
+					frequ = frequs(index)
+					
+					filename = 'zsavef/frequ_psth';
+					%gives us  'bpsth', 'bpsth_noref' 'repstot'
+					load(zcfilename(filename, num2str(frequ), fibertype, pressure_exp));
+					
+					%%% Q : normalize psth ? (/nrep)
+					
+					freptime = round(length(bpsth)*tdres);
+					
+					fourier = zcfourier(bpsth, tdres, freptime, calc);
+					fourier_noref = zcfourier(bpsth_noref, tdres, freptime, calc);
+					
+					if takeAbs == 1
+						normal = [ normal abs(fourier)];
+						noref = [ noref abs(fourier_noref)];
+						
+						graphf = 'Norm';
+					else
+						normal = [ normal angle(fourier)];
+						noref = [ noref angle(fourier_noref)];
+						
+						graphf = 'Angle';
+					end
+					
+				end
+				corrStr = [' fourier ' num2str(calc)];
+				
+				figure
+				plot(frequs, normal, 'b', frequs, noref, 'g');
+				title([graphf corrStr])
+				legend('normal', 'no ref');
+				xlabel('Frequ Hz');
+				ylabel([graphf corrStr]);
+					
+				figure
+				plot(frequs, normal ./ noref);
+				title([graphf corrStr '/' corrStr ' noref'])
+				xlabel('Frequ Hz');
+				ylabel([graphf corrStr '/' corrStr ' noref']);
+				
+			end	
 		
+		end
+		repstot
 	
 	else
 		for index=10:10:length(frequs)
 			frequ = frequs(index)
 					
-			filename = 'zsavef/frequ_psth'
+			filename = 'zsavef/frequ_psth';
 			%gives us  'bpsth', 'bpsth_noref' 'repstot'
 			load(zcfilename(filename, num2str(frequ), fibertype, pressure_exp));
 			
-			reptots
+			
 			gentitle = ['pure tone ' num2str(frequ)];
 			
 			zgpsthgraph(bpsth, bpsth_noref, (reptime - 1), repstot, tdres, gentitle);
 		end
-		
+		repstot
 	end		
 end
